c6f01d92890f1464a5119952dc196a54a62e9504
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 02:28:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = { :key => value}
-  return my_hash
+  my_hashp = { :key => value}
+  return my_hashp
 end
 
 def pioneer
